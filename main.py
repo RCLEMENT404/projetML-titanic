2e06c889889e184c0c4eac26eb4818210846e614
Raphaël <clement_raphael@orange.fr>
2022-10-16 15:06:07 +0200

    Forest img

--- main.py
+++ main.py
@@ -52,6 +52,8 @@ print(data.shape)
 
 X = data.drop(columns='survived')
 y = data["survived"]
+n_classes = y.shape[0]
+print(n_classes)
 plt.show()
 print(X.shape)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, train_size=0.7, random_state=random_state)
@@ -60,7 +62,7 @@ print(X_train.shape)
 print(X_test.shape)
 print(data.isnull().sum())
 
-max_depth_range = range(2,10)
+max_depth_range = range(2, 10)
 param_grid = {'criterion': ["gini", "entropy"], 'splitter': ["best"], 'max_depth': max_depth_range}
 clf = GridSearchCV(DecisionTreeClassifier(random_state=random_state), param_grid, scoring="accuracy", n_jobs=-1,
                    verbose=1, cv=3)
@@ -92,6 +94,16 @@ affichage_resultat(y_train, y_pred_train, 'Train')
 y_pred_test = classifier.predict(X_test)
 affichage_resultat(y_test, y_pred_test, 'Test')
 
-# plot_tree(classifier)
-# plt.savefig("Forest.svg")
-# plt.show()
+plot_tree(classifier.estimators_[5],feature_names=X.columns, filled=True, rounded=True)
+plt.savefig("Forest.svg")
+plt.show()
+
+data = pd.read_csv('Titanic_Research_v6_v2.csv')
+categorical_col = data.select_dtypes('object')
+for i in categorical_col.columns:
+    le = preprocessing.LabelEncoder()
+    le.fit_transform(categorical_col[i])
+    data[i] = le.transform(data[i])
+
+y_pred = classifier.predict(data)
+print(y_pred)
